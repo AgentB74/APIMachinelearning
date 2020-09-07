6e037de3f98a8a17b6af7ce9cd15a9d17996bf06
Andrew <pisankin96@mail.ru>
2020-09-07 21:51:47 +0700

    Small update "m_learning" file

--- m_learning/urls.py
+++ m_learning/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 # from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
 from robot import urls as robot_url
 from learning_model import urls as learning_model_url
 
@@ -28,6 +26,3 @@ urlpatterns = [
     path('model/', include(learning_model_url)),
     # url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico'), name='favicon'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
